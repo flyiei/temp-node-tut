cbb3df9a2c1687e15cdd48932a2ae3055ac38a59
Jason Li <jasonli@Jasons-MacBook-Pro.local>
2022-06-28 07:28:21 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ * npm init 
+ * npm init -y  --everything default
+ * 
+ * npm i <package name> -- local dependency for this particular project.
+ * npm install -g <package name> --install global dependency.
+ * sudo install -g <package name>
+ * 
+ */
+
+dddd
